457faea0f4949064336f93003379635224d3e7cb
yydaniel1999 <yydaniel1999@gmail.com>
2023-05-30 19:01:17 -0300

    criado o layout

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css'],
+})
+export class QuizzComponent {}
